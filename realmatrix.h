ef3fa9b7912508797f0c4a2ec9fa80c8a7edba90
ouzdeville <ouzdeville@gmail.com>
2024-04-30 10:19:59 +0000

    Update realmatrix.h
    amoul ragal

--- realmatrix.h
+++ realmatrix.h
@@ -13,5 +13,11 @@ rmat init_Id(int ordre);
 rmat add(rmat A, rmat B);
 rmat mult(rmat A, rmat B);
 rmat transposition(rmat A);
-
+rmat inverse(rmat A);
+float det(rmat A);
+float trace(rmat A);
+rmat pivot(rmat A);
+int rang(rmat A);
+float* valeurPropre(rmat A);
+int dimEspacePropre(rmat A, float lamda);
 #endif
