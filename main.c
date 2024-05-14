8442156ac2b767d8dc4f747db66cbccdd0c6dbe3
RokhyatouSF <rokhyatousarrfall@gmail.com>
2024-05-14 00:26:24 +0000

    implemetation de fonction 1

--- main.c
+++ main.c
@@ -1,17 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "realmatrix.h"
 
+void remplir_matrice(rmat mat, float valeur) {
+    for (int i = 0; i < mat.rown; i++) {
+        for (int j = 0; j < mat.coln; j++) {
+            mat.coeff[i][j] = valeur;
+        }
+    }
+}
 
-int main()
-{
-    int ligne, colonne;
-    printf("Donner le nmbre de lignes");
-    scanf("%d",&ligne);
-    printf("Donner le nmbre de colonnes");
-    scanf("%d",&colonne);
-    rmat M=init(ligne,colonne);
-    affiche(M);
-    
-    return 1;
+int main(int argc, char *argv[]) {
+    if (argc < 6) {
+        printf("Usage: %s <commande> -l <ligne> -n <colonne> [options]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int ligne = 0, colonne = 0;
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "-l") == 0) {
+            ligne = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-n") == 0) {
+            colonne = atoi(argv[i + 1]);
+        }
+    }
+
+    if (ligne <= 0 || colonne <= 0) {
+        printf("Erreur: Les dimensions de la matrice doivent être des entiers positifs.\n");
+        return EXIT_FAILURE;
+    }
+
+    rmat A, B, C;
+    if (strcmp(argv[1], "-init") == 0) {
+        A = init(ligne, colonne);
+        printf("Matrice initialisée:\n");
+        affiche(A);
+    } else if (strcmp(argv[1], "-init_Id") == 0) {
+        A = init_Id(ligne);
+        printf("Matrice identité initialisée:\n");
+        affiche(A);
+    } else if (strcmp(argv[1], "-add") == 0) {
+        A = init(ligne, colonne);
+        B = init(ligne, colonne);
+        remplir_matrice(A, 1.0); // Remplir A avec des 1 pour test
+        remplir_matrice(B, 2.0); // Remplir B avec des 2 pour test
+        C = add(A, B);
+        printf("Résultat de l'addition:\n");
+        affiche(C);
+    } else if (strcmp(argv[1], "-mult") == 0) {
+        A = init(ligne, colonne);
+        B = init(colonne, ligne);
+        remplir_matrice(A, 1.0); // Remplir A avec des 1 pour test
+        remplir_matrice(B, 2.0); // Remplir B avec des 2 pour test
+        C = mult(A, B);
+        printf("Résultat de la multiplication:\n");
+        affiche(C);
+    } else if (strcmp(argv[1], "-tr") == 0) {
+        A = init(ligne, colonne);
+        remplir_matrice(A, 1.0); // Remplir A avec des 1 pour test
+        float tr = trace(A);
+        printf("Trace de la matrice: %0.2f\n", tr);
+    } else {
+        printf("Commande non reconnue.\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
--- realmatrix.c
+++ realmatrix.c
@@ -1,43 +1,110 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "realmatrix.h"
 
-
-
-rmat init(int ligne, int colonne){
-
+rmat init(int ligne, int colonne) {
     rmat mat;
-    int i=0;
-    mat.rown=ligne;
-    mat.coln=colonne;
-    // je déclare un tableau contenant des tableaux (float *)
-    // les lignes sont des tablaux de tablaux
-    mat.coeff=(float **) malloc(mat.rown *sizeof(float *));
-    for ( i = 0; i < mat.rown; i++)
-    {
-        // chaque ligne est un tableau de réel
-        mat.coeff[i]= calloc(mat.coln, sizeof(float));
+    mat.rown = ligne;
+    mat.coln = colonne;
+    mat.coeff = (float **)malloc(mat.rown * sizeof(float *));
+    for (int i = 0; i < mat.rown; i++) {
+        mat.coeff[i] = calloc(mat.coln, sizeof(float));
     }
-
     return mat;
 }
 
-void affiche(rmat A){
-    int i,j;
-    for ( i = 0; i < A.rown; i++)
-    {
-        for ( j = 0; j < A.coln; j++)
-        {
+void affiche(rmat A) {
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
             printf("%0.2f \t", A.coeff[i][j]);
         }
         printf("\n");
-        
     }
-    
-
 }
-int inverse(rmat A, rmat invA){
 
-    
+rmat init_Id(int ordre) {
+    rmat mat = init(ordre, ordre);
+    for (int i = 0; i < ordre; i++) {
+        mat.coeff[i][i] = 1;
+    }
+    return mat;
+}
+
+rmat add(rmat A, rmat B) {
+    if (A.rown != B.rown || A.coln != B.coln) {
+        printf("Erreur: Les matrices doivent avoir les mêmes dimensions pour l'addition.\n");
+        exit(EXIT_FAILURE);
+    }
+    rmat result = init(A.rown, A.coln);
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
+            result.coeff[i][j] = A.coeff[i][j] + B.coeff[i][j];
+        }
+    }
+    return result;
+}
+
+rmat mult(rmat A, rmat B) {
+    if (A.coln != B.rown) {
+        printf("Erreur: Le nombre de colonnes de la première matrice doit être égal au nombre de lignes de la deuxième matrice pour la multiplication.\n");
+        exit(EXIT_FAILURE);
+    }
+    rmat result = init(A.rown, B.coln);
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < B.coln; j++) {
+            result.coeff[i][j] = 0;
+            for (int k = 0; k < A.coln; k++) {
+                result.coeff[i][j] += A.coeff[i][k] * B.coeff[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+rmat transposition(rmat A) {
+    rmat result = init(A.coln, A.rown);
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
+            result.coeff[j][i] = A.coeff[i][j];
+        }
+    }
+    return result;
+}
+
+int inverse(rmat A, rmat invA) {
+    // Implementation du calcul de l'inverse (à compléter)
+    return 0;
+}
+
+float det(rmat A) {
+    // Implementation du calcul du déterminant (à compléter)
+    return 0.0;
+}
+
+float trace(rmat A) {
+    float sum = 0.0;
+    for (int i = 0; i < A.rown && i < A.coln; i++) {
+        sum += A.coeff[i][i];
+    }
+    return sum;
+}
+
+rmat pivot(rmat A) {
+    // Implementation du pivotement (à compléter)
+    return A;
+}
+
+int rang(rmat A) {
+    // Implementation du calcul du rang (à compléter)
+    return 0;
+}
+
+float *valeurPropre(rmat A) {
+    // Implementation du calcul des valeurs propres (à compléter)
+    return NULL;
+}
+
+int dimEspacePropre(rmat A, float lamda) {
+    // Implementation du calcul de la dimension de l'espace propre (à compléter)
+    return 0;
 }
