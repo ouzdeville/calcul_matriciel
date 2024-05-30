0d23b91d0c2210805c7903b4b8a2bcf625f29409
Mamadou Issa Diedhiou <mamadouissadiedhiou701@gmail.com>
2024-05-30 14:31:31 +0000

    Travail fait

--- main.c
+++ main.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "realmatrix.h"
 
+int main() {
+    rmat A, B, C, T, invA;
+    int i, j;
+    srand(time(NULL));
 
-int main()
-{
-    int ligne, colonne;
-    printf("Donner le nmbre de lignes");
-    scanf("%d",&ligne);
-    printf("Donner le nmbre de colonnes");
-    scanf("%d",&colonne);
-    rmat M=init(ligne,colonne);
-    affiche(M);
-    
-    return 1;
+    A = init(2, 2);
+    B = init(2, 2);
+
+    for (i = 0; i < A.rown; i++) {
+        for (j = 0; j < A.coln; j++) {
+            A.coeff[i][j] = rand() % 2;
+            B.coeff[i][j] = rand() % 2;
+        }
+    }
+
+    printf("A =\n");
+    affiche(A);
+    printf("B =\n");
+    affiche(B);
+
+    C = mult(A, B);
+    printf("C (A * B) =\n");
+    affiche(C);
+    free(C.coeff);  
+
+    C = add(A, B);
+    printf("C (A + B) =\n");
+    affiche(C);
+    free(C.coeff); 
+
+    T = transposition(A);
+    printf("T (transposé de A) =\n");
+    affiche(T);
+    free(T.coeff); 
+
+    float determinant = det(A);
+    printf("det(A) = %0.2f\n", determinant);
+
+    int rank = rang(A);
+    printf("rank(A) = %d\n", rank);
+
+    invA = init(A.rown, A.coln);
+    int hasInverse = inverse(A, &invA);
+    if (hasInverse) {
+        printf("inv(A) =\n");
+        affiche(invA);
+    } else {
+        printf("A est singulière et n'a pas d'inverse.\n");
+    }
+    free(invA.coeff); 
+
+    free(A.coeff);
+    free(B.coeff);
+
+    return 0;
 }
