e98d2a097094bd25123ba1f2b06b716443dfac0e
sowmariama <sowmariame932@gmail.com>
2024-05-31 00:48:27 +0200

    Calcul MATRICIEL

--- main.c
+++ main.c
@@ -6,30 +6,60 @@
 
 int main()
 {
-    rmat A,B,C;
-    A=init(2,2);
-    B=init(2,2);
-    int i,j;
-    srand( time( NULL ) );
-    for ( i = 0; i < A.rown; i++)
+    rmat A, B, C;
+    A = init(2, 2);
+    B = init(2, 2);
+    int i, j;
+    srand(time(NULL));
+    for (i = 0; i < A.rown; i++)
     {
-        for ( j = 0; j < A.coln; j++)
+        for (j = 0; j < A.coln; j++)
         {
-            A.coeff[i][j]=((int)rand())%2;
-            B.coeff[i][j]=((int)rand())%2;
+            A.coeff[i][j] = ((int)rand()) % 2;
+            B.coeff[i][j] = ((int)rand()) % 2;
         }
-        
     }
-   
-    printf("A=\n");
+
+    
+    // Afficher les matrices A et B
+    printf("Matrice A :\n");
     affiche(A);
-    printf("B=\n");
+    printf("\nMatrice B :\n");
     affiche(B);
 
-    C=mult(A,B);
-    printf("C=\n");
+    // Addition des matrices A et B
+    printf("\nAddition des matrices A et B :\n");
+    C = add(A, B);
     affiche(C);
-    
-    
-    return 1;
+
+    // Multiplication des matrices A et B
+    printf("\nMultiplication des matrices A et B :\n");
+    C = mult(A, B);
+    affiche(C);
+
+    // Transposition de la matrice A
+    printf("\nTransposition de la matrice A :\n");
+    C = transposition(A);
+    affiche(C);
+
+    // Calcul du déterminant de la matrice A
+    printf("\nDeterminant de la matrice A : %f\n", determinant(A));
+
+    // Calcul du rang de la matrice A
+    printf("\nRang de la matrice A : %d\n", rang(A));
+
+    // Calcul de l'inverse de la matrice A
+    printf("\nInverse de la matrice A :\n");
+    if (inverse(A, &C)) {
+        affiche(C);
+    } else {
+        printf("La matrice A n'est pas inversible.\n");
+    }
+
+    // Libérer la mémoire
+    free_matrix(A);
+    free_matrix(B);
+    free_matrix(C);
+
+    return 0;
 }
--- realmatrix.c
+++ realmatrix.c
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "realmatrix.h"
 
 
@@ -34,7 +35,7 @@ rmat init_Id(int ordre){
     }
     return mat;
 }
-
+/*-------------------------------Fonction pour afficher la  matrice---------------------------------*/
 void affiche(rmat A){
     int i,j;
     for ( i = 0; i < A.rown; i++)
@@ -50,6 +51,26 @@ void affiche(rmat A){
 
 }
 
+/*-------------------------------Fonction pour liberer la memoire Allouer pour une matrice---------------------------------*/
+void free_matrix(rmat mat) {
+    for (int i = 0; i < mat.rown; i++) {
+        free(mat.coeff[i]);
+    }
+    free(mat.coeff);
+}
+
+/*-------------------------------Fonction pour remplir une matrice---------------------------------*/
+void fill_matrix(rmat *mat) {
+    for (int i = 0; i < mat->rown; i++) {
+        for (int j = 0; j < mat->coln; j++) {
+            printf("Entrez la valeur pour l'élément [%d][%d]: ", i, j);
+            scanf("%lf", &(mat->coeff[i][j]));
+        }
+    }
+}
+
+
+/*-------------------------------------Addition d'une matrice-----------------------------------------*/
 rmat add(rmat A, rmat B){
     rmat C=init(A.rown,A.coln);
     int i=0, j=0;
@@ -65,7 +86,7 @@ rmat add(rmat A, rmat B){
     return C;
 }
 
-
+/*-------------------------------------Multiplication d'une matrice-----------------------------------------*/
 rmat mult(rmat A, rmat B){
     rmat C=init(A.rown,B.coln);
     int i=0, j=0,k;
@@ -85,4 +106,223 @@ rmat mult(rmat A, rmat B){
     
     return C;
 
+}
+
+/*-------------------------------------Transposition d'une matrice-----------------------------------------*/
+
+rmat transposition(rmat A) {
+    rmat T = init(A.coln, A.rown);  // Dimensions inversées pour la transpose
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
+            T.coeff[j][i] = A.coeff[i][j];
+        }
+    }
+    return T;
+}
+
+/*-------------------------------------pivot d'une matrice-----------------------------------------------*/
+
+rmat pivot(rmat *A) {
+    int i, j, k;
+    int n = A->rown;
+    int m = A->coln;
+
+    for (i = 0; i < n; i++) {
+        // Trouver le pivot
+        double maxEl = fabs(A->coeff[i][i]);
+        int maxRow = i;
+        for (k = i + 1; k < n; k++) {
+            if (fabs(A->coeff[k][i]) > maxEl) {
+                maxEl = fabs(A->coeff[k][i]);
+                maxRow = k;
+            }
+        }
+
+        // Échanger les lignes
+        for (k = i; k < m; k++) {
+            double tmp = A->coeff[maxRow][k];
+            A->coeff[maxRow][k] = A->coeff[i][k];
+            A->coeff[i][k] = tmp;
+        }
+
+        // Mettre à 1 le pivot
+        for (k = i + 1; k < m; k++) {
+            A->coeff[i][k] /= A->coeff[i][i];
+        }
+        A->coeff[i][i] = 1.0;
+
+        // Rendre les autres éléments de la colonne égaux à 0
+        for (j = 0; j < n; j++) {
+            if (j != i) {
+                double c = A->coeff[j][i];
+                for (k = i; k < m; k++) {
+                    if (i == k) {
+                        A->coeff[j][k] = 0;
+                    } else {
+                        A->coeff[j][k] -= c * A->coeff[i][k];
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*-------------------------------------determinant d'une matrice-----------------------------------------------*/
+
+//Fonction pour oobtenir la matrice des cofacteurs
+rmat get_cofactor(rmat A, int p, int q, int n) {
+    int i = 0, j = 0;
+    rmat temp = init(n - 1, n - 1);
+
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            if (row != p && col != q) {
+                temp.coeff[i][j++] = A.coeff[row][col];
+
+                if (j == n - 1) {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    return temp;
+}
+
+// Fonction pour calculer le determinant d'une matrice
+float determinant(rmat A) {
+    if (A.rown != A.coln) {
+        fprintf(stderr, "Erreur : Le déterminant ne peut être calculé que pour les matrices carrées.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int n = A.rown;
+
+    if (n == 1) {
+        return A.coeff[0][0];
+    }
+
+    if (n == 2) {
+        return A.coeff[0][0] * A.coeff[1][1] - A.coeff[0][1] * A.coeff[1][0];
+    }
+
+    double det = 0;
+    int sign = 1;
+
+    for (int f = 0; f < n; f++) {
+        rmat temp = get_cofactor(A, 0, f, n);
+        det += sign * A.coeff[0][f] * determinant(temp);
+        sign = -sign;
+        free_matrix(temp);
+    }
+
+    return det;
+}
+
+/*---------------------------------------Inverse d'une matrice------------------------------------------------*/
+
+int inverse(rmat A, rmat *invA) {
+    if (A.rown != A.coln) {
+        fprintf(stderr, "Erreur : Seules les matrices carrées peuvent être inversées.\n");
+        return 0;  // Erreur
+    }
+
+    int n = A.rown;
+    rmat aug = init(n, 2 * n);
+
+    // Construire la matrice augmentée [A | I]
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            aug.coeff[i][j] = A.coeff[i][j];
+            aug.coeff[i][j + n] = (i == j) ? 1.0 : 0.0;
+        }
+    }
+
+    // Appliquer l'algorithme de Gauss-Jordan
+    for (int i = 0; i < n; i++) {
+        // Trouver le pivot
+        double maxEl = fabs(aug.coeff[i][i]);
+        int maxRow = i;
+        for (int k = i + 1; k < n; k++) {
+            if (fabs(aug.coeff[k][i]) > maxEl) {
+                maxEl = fabs(aug.coeff[k][i]);
+                maxRow = k;
+            }
+        }
+
+        // Échanger les lignes
+        for (int k = 0; k < 2 * n; k++) {
+            double tmp = aug.coeff[maxRow][k];
+            aug.coeff[maxRow][k] = aug.coeff[i][k];
+            aug.coeff[i][k] = tmp;
+        }
+
+        // Mettre à 1 le pivot
+        double pivot = aug.coeff[i][i];
+        if (pivot == 0) {
+            fprintf(stderr, "Erreur : Matrice non inversible.\n");
+            free_matrix(aug);
+            return 0;  // Erreur
+        }
+        for (int k = 0; k < 2 * n; k++) {
+            aug.coeff[i][k] /= pivot;
+        }
+
+        // Rendre les autres éléments de la colonne égaux à 0
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                double factor = aug.coeff[j][i];
+                for (int k = 0; k < 2 * n; k++) {
+                    aug.coeff[j][k] -= factor * aug.coeff[i][k];
+                }
+            }
+        }
+    }
+
+    // Extraire la matrice inverse
+    *invA = init(n, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            invA->coeff[i][j] = aug.coeff[i][j + n];
+        }
+    }
+
+    free_matrix(aug);
+    return 1;  // Succès
+}
+
+/*---------------------------------------Rang d'une matrice------------------------------------------------*/
+
+int rang(rmat A) {
+    int rank = 0;
+    int n = A.rown;
+    int m = A.coln;
+    
+    // Appliquer l'algorithme de Gauss-Jordan
+    for (int row = 0; row < n; row++) {
+        if (fabs(A.coeff[row][row]) > 1e-6) {
+            for (int col = 0; col < m; col++) {
+                if (col != row) {
+                    double factor = A.coeff[col][row] / A.coeff[row][row];
+                    for (int k = 0; k < m; k++) {
+                        A.coeff[col][k] -= factor * A.coeff[row][k];
+                    }
+                }
+            }
+        }
+    }
+
+    // Compter le nombre de lignes non nulles
+    for (int row = 0; row < n; row++) {
+        int nonZero = 0;
+        for (int col = 0; col < m; col++) {
+            if (fabs(A.coeff[row][col]) > 1e-6) {
+                nonZero = 1;
+                break;
+            }
+        }
+        rank += nonZero;
+    }
+
+    return rank;
 }
