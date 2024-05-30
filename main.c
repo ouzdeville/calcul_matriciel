66b6a58ca41e33d35bbc5e1100b9071cbcf2ecd0
Laremad <ndeyemaremediongue7@gmail.com>
2024-05-31 01:14:19 +0200

    ajout du dossier

--- main.c
+++ main.c
@@ -2,16 +2,81 @@
 #include <stdlib.h>
 #include "realmatrix.h"
 
-
-int main()
-{
+int main() {
     int ligne, colonne;
-    printf("Donner le nmbre de lignes");
-    scanf("%d",&ligne);
-    printf("Donner le nmbre de colonnes");
-    scanf("%d",&colonne);
-    rmat M=init(ligne,colonne);
+
+    printf("Donner le nombre de lignes : ");
+    scanf("%d", &ligne);
+    printf("Donner le nombre de colonnes : ");
+    scanf("%d", &colonne);
+
+    rmat M = init(ligne, colonne);
+
+    // Demandez à l'utilisateur de saisir les valeurs de la matrice M
+    printf("Remplissez la matrice M :\n");
+    for (int i = 0; i < ligne; i++) {
+        for (int j = 0; j < colonne; j++) {
+            printf("M[%d][%d] : ", i, j);
+            scanf("%f", &M.coeff[i][j]);
+        }
+    }
+
+    printf("Matrice M :\n");
     affiche(M);
-    
-    return 1;
+
+    printf("Entrez une autre matrice N pour l'addition (memes dimensions que M) :\n");
+    rmat N = init(ligne, colonne);
+    for (int i = 0; i < ligne; i++) {
+        for (int j = 0; j < colonne; j++) {
+            printf("N[%d][%d] : ", i, j);
+            scanf("%f", &N.coeff[i][j]);
+        }
+    }
+
+    printf("Somme de M et N :\n");
+    rmat sum = add(M, N);
+    affiche(sum);
+
+    printf("Entrez une autre matrice P pour la multiplication (nombre de colonnes de la 2e matrice doit etre egal au nombre de lignes de M) :\n");
+    int col_P;
+    printf("Donner le nombre de colonnes de la matrice pour multiplication : ");
+    scanf("%d", &col_P);
+
+    rmat P = init(colonne, col_P); // Adjust the dimensions accordingly
+    for (int i = 0; i < colonne; i++) {
+        for (int j = 0; j < col_P; j++) {
+            printf("P[%d][%d] : ", i, j);
+            scanf("%f", &P.coeff[i][j]);
+        }
+    }
+
+    printf("Produit de M et P :\n");
+    rmat product = mult(M, P);
+    affiche(product);
+
+    rmat transM = transposition(M);
+    printf("Matrice transposee de M :\n");
+    affiche(transM);
+
+    float determinant_M = det(M);
+    printf("Determinant de M : %0.6f\n", determinant_M);
+
+    rmat inv_M;
+    int success = inverse(M, &inv_M);
+    if (success) {
+        printf("Inverse de M :\n");
+        affiche(inv_M);
+        free(inv_M.coeff);
+    } else {
+        printf("La matrice M n'est pas inversible.\n");
+    }
+
+    free(M.coeff);
+    free(N.coeff);
+    free(P.coeff);
+    free(sum.coeff);
+    free(product.coeff);
+    free(transM.coeff);
+
+    return 0;
 }
--- realmatrix.c
+++ realmatrix.c
@@ -1,43 +1,173 @@
-
+#include "realmatrix.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "realmatrix.h"
 
-
-
-rmat init(int ligne, int colonne){
-
+rmat init(int ligne, int colonne) {
     rmat mat;
-    int i=0;
-    mat.rown=ligne;
-    mat.coln=colonne;
-    // je déclare un tableau contenant des tableaux (float *)
-    // les lignes sont des tablaux de tablaux
-    mat.coeff=(float **) malloc(mat.rown *sizeof(float *));
-    for ( i = 0; i < mat.rown; i++)
-    {
-        // chaque ligne est un tableau de réel
-        mat.coeff[i]= calloc(mat.coln, sizeof(float));
+    mat.rown = ligne;
+    mat.coln = colonne;
+    mat.coeff = (float **)malloc(mat.rown * sizeof(float *));
+    for (int i = 0; i < mat.rown; i++) {
+        mat.coeff[i] = (float *)calloc(mat.coln, sizeof(float));
     }
-
     return mat;
 }
 
-void affiche(rmat A){
-    int i,j;
-    for ( i = 0; i < A.rown; i++)
-    {
-        for ( j = 0; j < A.coln; j++)
-        {
-            printf("%0.2f \t", A.coeff[i][j]);
+rmat init_Id(int ordre) {
+    rmat mat = init(ordre, ordre);
+    for (int i = 0; i < ordre; i++) {
+        mat.coeff[i][i] = 1.0;
+    }
+    return mat;
+}
+
+rmat add(rmat A, rmat B) {
+    if (A.rown != B.rown || A.coln != B.coln) {
+        printf("Erreur: les matrices doivent avoir les memes dimensions pour etre additionnees.\n");
+        return init(0, 0); // Return an empty matrix on error
+    }
+
+    rmat C = init(A.rown, A.coln);
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
+            C.coeff[i][j] = A.coeff[i][j] + B.coeff[i][j];
+        }
+    }
+    return C;
+}
+
+rmat mult(rmat A, rmat B) {
+    if (A.coln != B.rown) {
+        printf("Erreur: les dimensions internes doivent etre egales pour la multiplication.\n");
+        return init(0, 0); // Return an empty matrix on error
+    }
+
+    rmat C = init(A.rown, B.coln);
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < B.coln; j++) {
+            for (int k = 0; k < A.coln; k++) {
+                C.coeff[i][j] += A.coeff[i][k] * B.coeff[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+void affiche(rmat A) {
+    for (int i = 0; i < A.rown; i++) {
+        for (int j = 0; j < A.coln; j++) {
+            printf("%0.2f\t", A.coeff[i][j]);
         }
         printf("\n");
-        
     }
-    
-
 }
-int inverse(rmat A, rmat invA){
 
-    
+rmat transposition(rmat A) {
+    rmat trans = init(A.coln, A.rown);
+    for (int i = 0; i < A.coln; i++) {
+        for (int j = 0; j < A.rown; j++) {
+            trans.coeff[i][j] = A.coeff[j][i];
+        }
+    }
+    return trans;
+}
+
+float det(rmat A) {
+    if (A.rown != A.coln) {
+        printf("Le determinant ne peut etre calcule que pour une matrice carree.\n");
+        return 0.0;
+    }
+    if (A.rown == 1) {
+        return A.coeff[0][0];
+    }
+    if (A.rown == 2) {
+        return A.coeff[0][0] * A.coeff[1][1] - A.coeff[0][1] * A.coeff[1][0];
+    }
+    float determinant = 0.0;
+    for (int k = 0; k < A.coln; k++) {
+        rmat submatrix = init(A.rown - 1, A.coln - 1);
+        for (int i = 1; i < A.rown; i++) {
+            int subi = 0;
+            for (int j = 0; j < A.coln; j++) {
+                if (j == k) continue;
+                submatrix.coeff[i - 1][subi++] = A.coeff[i][j];
+            }
+        }
+        determinant += (k % 2 == 0 ? 1 : -1) * A.coeff[0][k] * det(submatrix);
+        for (int i = 0; i < submatrix.rown; i++) {
+            free(submatrix.coeff[i]);
+        }
+        free(submatrix.coeff);
+    }
+    return determinant;
+}
+
+int inverse(rmat A, rmat *invA) {
+    if (A.rown != A.coln) {
+        printf("La matrice doit être carree pour pouvoir etre inversee.\n");
+        return 0;
+    }
+
+    int n = A.rown;
+    rmat identity = init_Id(n);
+    rmat temp = init(n, n);
+
+    // Copy A to temp
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp.coeff[i][j] = A.coeff[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int pivot_row = i;
+        while (pivot_row < n && temp.coeff[pivot_row][i] == 0) {
+            pivot_row++;
+        }
+        if (pivot_row == n) {
+            printf("La matrice n'est pas inversible.\n");
+            for (int i = 0; i < n; i++) {
+                free(temp.coeff[i]);
+                free(identity.coeff[i]);
+            }
+            free(temp.coeff);
+            free(identity.coeff);
+            return 0;
+        }
+
+        if (pivot_row != i) {
+            float *temp_row = temp.coeff[i];
+            temp.coeff[i] = temp.coeff[pivot_row];
+            temp.coeff[pivot_row] = temp_row;
+
+            float *identity_row = identity.coeff[i];
+            identity.coeff[i] = identity.coeff[pivot_row];
+            identity.coeff[pivot_row] = identity_row;
+        }
+
+        float pivot = temp.coeff[i][i];
+        for (int j = 0; j < n; j++) {
+            temp.coeff[i][j] /= pivot;
+            identity.coeff[i][j] /= pivot;
+        }
+
+        for (int k = 0; k < n; k++) {
+            if (k != i) {
+                float factor = temp.coeff[k][i];
+                for (int j = 0; j < n; j++) {
+                    temp.coeff[k][j] -= factor * temp.coeff[i][j];
+                    identity.coeff[k][j] -= factor * identity.coeff[i][j];
+                }
+            }
+        }
+    }
+
+    *invA = identity;
+
+    for (int i = 0; i < n; i++) {
+        free(temp.coeff[i]);
+    }
+    free(temp.coeff);
+
+    return 1;
 }
