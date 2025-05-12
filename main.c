2647599dd22ff9936743e9f5125b3632eea53b86
pepitedk <diakhalakonte97@gmail.com>
2025-05-12 13:59:49 +0000

    mon devoir

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main()
     A=init(2,2);
     B=init(2,2);
     int i,j;
+
     srand( time( NULL ) );
     for ( i = 0; i < A.rown; i++)
     {
@@ -20,16 +21,38 @@ int main()
         }
         
     }
-   
+
     printf("A=\n");
     affiche(A);
     printf("B=\n");
-    affiche(B);
+    affiche(B);  
 
-    C=mult(A,B);
+    /*C=mult(A,B);
+    printf("C=\n");
+    affiche(C); */
+
+    C = add(A, B);
+    printf("C = A + B =\n");
+    affiche(C);
+
+ C = mult(A, B);
     printf("C=\n");
     affiche(C);
     
-    
+   rmat T = transposition(A);
+    printf("Transposee de A =\n");
+    affiche(T);
     return 1;
+
+    rmat inv = init(2,2);
+    printf("Inverse de A :\n");
+    if(inverse(A, inv)) {
+        affiche(inv);
+    }else {
+        printf("Matrice non inversible.\n");
+    }
+
+
+float d = det(A);
+printf("Determinant de A : %.2f\n", d);
 }
--- realmatrix.c
+++ realmatrix.c
@@ -46,7 +46,136 @@ void affiche(rmat A){
         printf("\n");
         
     }
-    
+}
+//addition
+   rmat add(rmat A, rmat B) {
+        rmat C;
+        int i, j;
 
+        if (A.rown != B.rown || A.coln != B.coln) {
+            printf("Erreur : dimensions incompatibles pour l'addition\n");
+            exit(1);
+        }
+        C = init(A.rown, A.coln);
+        for (i = 0; i < A.rown; i++) {
+            for(j = 0; j < A.coln; j++) {
+                C.coeff[i][j] = A.coeff[i][j] + B.coeff[i][j];
+            }
+        }
+        return C;
+    }
+
+    // multiplication
+   rmat mult(rmat A, rmat B) {
+        if (A.coln != B.rown)
+        {
+            printf("\n Mutltiplication impossible\n");
+            exit(-1);
+        }
+        rmat C = init(A.rown,B.coln);
+        for (int i = 0; i < A.rown; i++)
+        {
+            for (int j = 0; j < B.coln;j++){
+                int tmp = 0;
+                for (int k = 0;k < A.coln; k++){
+                    tmp += A.coeff[i][k]*B.coeff[k][j];
+                }
+                C.coeff[i][j] = tmp;
+            }
+        }
+        return C;
+    }
+
+    // transposition
+   rmat transposition(rmat A) {
+        rmat C = init(A.coln,A.rown);
+        for(int i = 0; i < A.rown;i++){
+            for (int j = 0; j < A.coln; j++){
+                C.coeff[j][i] = A.coeff[i][j];
+            }
+        }
+        return C;
+    }
+
+    //inverse
+    int inverse(rmat A, rmat invA) {
+        if (A.rown != A.coln) {
+            printf("Erreur : la matrice n'est pas carree\n");
+            return 0;
+        }
+
+        int n = A.rown;
+        int i,j,k;
+        float pivot, ratio;
+
+        rmat temp = init(n, n);
+        for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+    temp.coeff[i][j] = A.coeff[i][j];
+
+    for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+         invA.coeff[i][j] = (i == j) ? 1.0 : 0.0;
+
+    for (i = 0; i < n; i++) {
+        float pivot = temp.coeff[i][i];
+        if (pivot == 0.0) {
+            printf("pivot nul : matrice non inversible\n");
+            return 0;
+        }
+
+        for (j = 0; j < n; j++) {
+            temp.coeff[i][j] /= pivot;
+            invA.coeff[i][j] /= pivot;
+        }
+
+        for (k = 0; k < n; k++) {
+            if (k != i) {
+                ratio = temp.coeff[k][i];
+                for (j = 0; j < n; j++) {
+                temp.coeff[k][j] -= ratio * temp.coeff[i][j];
+            invA.coeff[k][j] -= ratio * invA.coeff[i][j];
+         }
+        }
+    }
+}
+return 1;
 }
 
+//determinant
+float det(rmat A) {
+    if (A.rown != A.coln) {
+        printf("Erreur : matrice non caree\n");
+        return 0.0;
+    }
+    int n = A.rown;
+
+    if (n == 1) {
+        return A.coeff[0][0];
+    }
+    if ( n == 2) {
+        return A.coeff[0][0] * A.coeff[1][1] - A.coeff[0][1] * A.coeff[1][0];
+
+    }
+    float determinant = 0.0;
+    for (int k = 0; k < n; k++) {
+        rmat sub = init(n - 1, n - 1);
+        for (int i = 1; i < n; i++) {
+            int col = 0;
+            for (int j = 0; j < n; j++) {
+            if ( j == k) continue;
+            sub.coeff[i - 1][col] = A.coeff[i][j];
+            col++;
+        }
+    }
+    float sign = (k % 2 == 0) ? 1.0 : -1.0;
+    determinant += sign * A.coeff[0][k] * det(sub);
+
+    for (int i = 0; i < sub.rown; i++)
+    free(sub.coeff[i]);
+free(sub.coeff);
+}
+return determinant;
+}
+
+ 
