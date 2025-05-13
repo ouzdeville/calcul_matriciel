3f52a89bbf7221908e381e985459f9835f8ff75d
MO-TINE <tinemamadou636@gmail.com>
2025-05-14 00:44:21 +0100

    implementation

--- main.c
+++ main.c
@@ -6,30 +6,44 @@
 
 int main()
 {
-    rmat A,B,C;
+    rmat A,B,C,D,T,invA;
+    float detA;
     A=init(2,2);
     B=init(2,2);
     int i,j;
     srand( time( NULL ) );
-    for ( i = 0; i < A.rown; i++)
-    {
-        for ( j = 0; j < A.coln; j++)
-        {
-            A.coeff[i][j]=((int)rand())%2;
-            B.coeff[i][j]=((int)rand())%2;
+    for ( i = 0; i < A.rown; i++){
+        for ( j = 0; j < A.coln; j++){
+            A.coeff[i][j]=((int)rand())%6;
+            B.coeff[i][j]=((int)rand())%6;
         }
         
     }
    
-    printf("A=\n");
+    printf("Voici la matrice A : \n");
     affiche(A);
-    printf("B=\n");
+    printf("Voici la matrice B : \n");
     affiche(B);
 
-    C=mult(A,B);
-    printf("C=\n");
+    C=add(A,B);
+    printf("Voici l'addition des matrices A et B\n");
     affiche(C);
+
+    D=mult(A,B);
+    printf("Voici la multiplication des matrices A et B\n");
+    affiche(D);
     
+    T=transposition(A);
+    printf("Voici la transposition de la matrice A\n");
+    affiche(T);
+
+    detA=det(A);
+    printf("Voici le determinant de la matrice A\n");
+    printf("%f\n",detA);
+
+    invA=inverse(A);
+    printf("Voici l'inverse de la matrice A\n");
+    affiche(invA);
     
     return 1;
 }
--- realmatrix.c
+++ realmatrix.c
@@ -46,7 +46,90 @@ void affiche(rmat A){
         printf("\n");
         
     }
-    
 
+}
+
+rmat add(rmat A, rmat B){
+    rmat C;
+    int i,j;
+    if (A.rown!=B.rown || A.coln!=B.coln){
+        printf("l'addition de deux matrices n'est possible que si les deux matrices ont le même nombre de lignes et de colonnes\n");
+        exit(1);
+    }
+    C=init(A.rown, A.coln);
+    for ( i = 0; i < A.rown; i++){
+        for ( j = 0; j < A.coln; j++){
+            C.coeff[i][j]=A.coeff[i][j]+B.coeff[i][j];
+        }
+        
+    }
+    return C;
+}
+
+rmat mult(rmat A, rmat B){
+    rmat D;
+    int i,j,k;
+    if (A.coln!=B.rown){
+        printf("la multiplication de deux matrices n'est possible que si le nombre de colonnes de la première A.coeff est égal au nombre de lignes de la deuxième A.coeff\n");
+        exit(1);
+    }
+    D=init(A.rown, B.coln);
+    for ( i = 0; i < A.rown; i++){
+        for ( j = 0; j < B.coln; j++){
+            D.coeff[i][j]=0;
+            for ( k = 0; k < A.coln; k++){
+                D.coeff[i][j]+=A.coeff[i][k]*B.coeff[k][j];
+            }
+        }
+        
+    }
+    return D;
+}
+
+rmat transposition(rmat A){
+    rmat T;
+    int i,j;
+    T=init(A.coln, A.rown);
+    for ( i = 0; i < A.rown; i++){
+        for ( j = 0; j < A.coln; j++){
+            T.coeff[j][i]=A.coeff[i][j];
+        }
+        
+    }
+    return T;
+}
+
+float det(rmat A){
+    float det;
+    if (A.rown!=A.coln){
+        printf("le déterminant n'est défini que pour les matrices carrées\n");
+        exit(1);
+    }else {
+        det=A.coeff[0][0]*A.coeff[1][1]-A.coeff[0][1]*A.coeff[1][0];
+    }
+        
+    return det;
+}
+
+rmat inverse(rmat A){
+    rmat invA;
+    float detA;
+    if (A.rown!=A.coln){
+        printf("l'inverse n'est défini que pour les matrices carrées\n");
+        exit(1);
+    }else {
+        detA=det(A);
+        if (detA==0){
+            printf("la matrices n'est pas inversible\n");
+            exit(1);
+        }
+    }
+    invA=init(A.rown, A.coln);
+     invA.coeff[0][0] = A.coeff[1][1] / detA;
+     invA.coeff[0][1] = -A.coeff[0][1] / detA;
+     invA.coeff[1][0] = -A.coeff[1][0] / detA;
+     invA.coeff[1][1] = A.coeff[0][0] / detA;
+   
+    return invA;
 }
 
