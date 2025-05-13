c57a0d61aff14f70abce338ba5447e2972f058de
ndeyefatoumataba <ndeyefatoumataba98@gmail.com>
2025-05-13 14:13:29 -0700

    commiter ajouter

--- main.c
+++ main.c
@@ -3,33 +3,336 @@
 #include <time.h>
 #include "realmatrix.h"
 
-
 int main()
 {
-    rmat A,B,C;
-    A=init(2,2);
-    B=init(2,2);
-    int i,j;
-    srand( time( NULL ) );
-    for ( i = 0; i < A.rown; i++)
-    {
-        for ( j = 0; j < A.coln; j++)
-        {
-            A.coeff[i][j]=((int)rand())%2;
-            B.coeff[i][j]=((int)rand())%2;
+    rmat A, B, C, T;
+    int i, j;
+
+    // Demander les dimensions des matrices
+    int rows, cols;
+    printf("Entrez le nombre de lignes des matrices : ");
+    scanf("%d", &rows);
+    printf("Entrez le nombre de colonnes des matrices : ");
+    scanf("%d", &cols);
+
+    // Initialiser les matrices A et B
+    A = init(rows, cols);
+    B = init(rows, cols);
+
+    // Demander à l'utilisateur de remplir la matrice A
+    printf("Entrez les éléments de la matrice A :\n");
+    for (i = 0; i < A.rown; i++) {
+        for (j = 0; j < A.coln; j++) {
+            printf("A[%d][%d] = ", i, j);
+            scanf("%f", &A.coeff[i][j]);
         }
-        
     }
-   
-    printf("A=\n");
+
+    // Demander à l'utilisateur de remplir la matrice B
+    printf("Entrez les éléments de la matrice B :\n");
+    for (i = 0; i < B.rown; i++) {
+        for (j = 0; j < B.coln; j++) {
+            printf("B[%d][%d] = ", i, j);
+            scanf("%f", &B.coeff[i][j]);
+        }
+    }
+
+    // Afficher les matrices A et B
+    printf("Matrice A :\n");
     affiche(A);
-    printf("B=\n");
+
+    printf("Matrice B :\n");
     affiche(B);
 
-    C=mult(A,B);
-    printf("C=\n");
+    // Additionner les matrices A et B
+    C = add(A, B);
+
+    // Afficher la matrice résultat C
+    printf("Matrice C (A + B) :\n");
     affiche(C);
+
+  // Multiplier les matrices A et B
+   C = mult(A, B);
+
+   // Afficher la matrice résultat 
+   printf("Matrice C (A * B) :\n");
+     affiche(C);
+
+
+    // Calculer la transposée de la matrice A
+    T = transposition(A);
+
+    // Afficher la matrice transposée T
+    printf("Matrice T (transposée de A) :\n");
+    affiche(T);
+
+    rmat invA;
     
     
-    return 1;
+
+    // Demander la taille de la matrice carrée
+    printf("Entrez la taille de la matrice carrée : ");
+    int n;
+    scanf("%d", &n);
+
+    // Initialiser les matrices A et invA
+    A = init(n, n);
+    invA = init(n, n);
+
+    
+
+    // Afficher la matrice A
+    printf("Matrice A :\n");
+    affiche(A);
+
+    // Calculer l'inverse de A
+    if (inverse(A, invA)) {
+        // Afficher la matrice inverse
+        printf("Matrice inverse de A :\n");
+        affiche(invA);
+    } else {
+        // Si la matrice n'est pas inversible
+        printf("La matrice A n'est pas inversible.\n");
+    }
+
+    // Calculer le déterminant de A
+    float determinant = det(A);
+    printf("Le déterminant de la matrice A est : %0.2f\n", determinant);
+   // Calculer la trace de A
+   float tr = trace(A);
+   printf("La trace de la matrice A est : %0.2f\n", tr);
+   //calculer rmat pivot(rmat A)
+   rmat P = pivot(A);
+   printf("Matrice P (matrice échelonnée de A) :\n");
+   affiche(P);
+    // Calculer le rang de A
+    int rank = rang(A);
+    printf("Le rang de la matrice A est : %d\n", rank);
+    // Calculer les valeurs propres de A
+    return 0;
+
 }
+
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "realmatrix.h"
+
+// Fonction pour afficher le menu
+void afficher_menu() {
+    printf("\n=== MENU ===\n");
+    printf("1. Additionner deux matrices\n");
+    printf("2. Multiplier deux matrices\n");
+    printf("3. Calculer la transposée d'une matrice\n");
+    printf("4. Calculer l'inverse d'une matrice\n");
+    printf("5. Calculer le déterminant d'une matrice\n");
+    printf("6. Calculer la trace d'une matrice\n");
+    printf("7. Calculer la matrice échelonnée (pivot de Gauss)\n");
+    printf("8. Quitter\n");
+    printf("Choisissez une option : ");
+}
+
+int main() {
+    rmat A, B, C, T, invA, P;
+    int i, j, choix;
+    int rows, cols;
+
+    while (1) {
+        afficher_menu();
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1: // Additionner deux matrices
+                printf("Entrez le nombre de lignes des matrices : ");
+                scanf("%d", &rows);
+                printf("Entrez le nombre de colonnes des matrices : ");
+                scanf("%d", &cols);
+
+                A = init(rows, cols);
+                B = init(rows, cols);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                printf("Entrez les éléments de la matrice B :\n");
+                for (i = 0; i < B.rown; i++) {
+                    for (j = 0; j < B.coln; j++) {
+                        printf("B[%d][%d] = ", i, j);
+                        scanf("%f", &B.coeff[i][j]);
+                    }
+                }
+
+                C = add(A, B);
+                printf("Matrice C (A + B) :\n");
+                affiche(C);
+                // liberer_memoire(A);
+                // liberer_memoire(B);
+                // liberer_memoire(C);
+                break;
+
+            case 2: // Multiplier deux matrices
+                printf("Entrez le nombre de lignes de la matrice A : ");
+                scanf("%d", &rows);
+                printf("Entrez le nombre de colonnes de la matrice A : ");
+                scanf("%d", &cols);
+
+                A = init(rows, cols);
+
+                printf("Entrez le nombre de colonnes de la matrice B : ");
+                int colsB;
+                scanf("%d", &colsB);
+
+                B = init(cols, colsB);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                printf("Entrez les éléments de la matrice B :\n");
+                for (i = 0; i < B.rown; i++) {
+                    for (j = 0; j < B.coln; j++) {
+                        printf("B[%d][%d] = ", i, j);
+                        scanf("%f", &B.coeff[i][j]);
+                    }
+                }
+
+                C = mult(A, B);
+                printf("Matrice C (A * B) :\n");
+                affiche(C);
+                // liberer_memoire(A);
+                // liberer_memoire(B);
+                // liberer_memoire(C);
+                break;
+
+            case 3: // Calculer la transposée d'une matrice
+                printf("Entrez le nombre de lignes de la matrice : ");
+                scanf("%d", &rows);
+                printf("Entrez le nombre de colonnes de la matrice : ");
+                scanf("%d", &cols);
+
+                A = init(rows, cols);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                T = transposition(A);
+                printf("Matrice T (transposée de A) :\n");
+                affiche(T);
+                // liberer_memoire(A);
+                // liberer_memoire(T);
+                break;
+
+            case 4: // Calculer l'inverse d'une matrice
+                printf("Entrez la taille de la matrice carrée : ");
+                scanf("%d", &rows);
+
+                A = init(rows, rows);
+                invA = init(rows, rows);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                if (inverse(A, invA)) {
+                    printf("Matrice inverse de A :\n");
+                    affiche(invA);
+                } else {
+                    printf("La matrice A n'est pas inversible.\n");
+                }
+                // liberer_memoire(A);
+                // liberer_memoire(invA);
+                break;
+
+            case 5: // Calculer le déterminant d'une matrice
+                printf("Entrez la taille de la matrice carrée : ");
+                scanf("%d", &rows);
+
+                A = init(rows, rows);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                float determinant = det(A);
+                printf("Le déterminant de la matrice A est : %0.2f\n", determinant);
+                // liberer_memoire(A);
+                break;
+
+            case 6: // Calculer la trace d'une matrice
+                printf("Entrez la taille de la matrice carrée : ");
+                scanf("%d", &rows);
+
+                A = init(rows, rows);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                float tr = trace(A);
+                printf("La trace de la matrice A est : %0.2f\n", tr);
+                // liberer_memoire(A);
+                break;
+
+            case 7: // Calculer la matrice échelonnée
+                printf("Entrez le nombre de lignes de la matrice : ");
+                scanf("%d", &rows);
+                printf("Entrez le nombre de colonnes de la matrice : ");
+                scanf("%d", &cols);
+
+                A = init(rows, cols);
+
+                printf("Entrez les éléments de la matrice A :\n");
+                for (i = 0; i < A.rown; i++) {
+                    for (j = 0; j < A.coln; j++) {
+                        printf("A[%d][%d] = ", i, j);
+                        scanf("%f", &A.coeff[i][j]);
+                    }
+                }
+
+                P = pivot(A);
+                printf("Matrice P (matrice échelonnée de A) :\n");
+                affiche(P);
+                // liberer_memoire(A);
+                // liberer_memoire(P);
+                break;
+
+            case 8: // Quitter
+                printf("Au revoir !\n");
+                exit(0);
+
+            default:
+                printf("Option invalide. Veuillez réessayer.\n");
+        }
+    }
+
+    return 0;
+}*/
--- realmatrix.c
+++ realmatrix.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "realmatrix.h"
+#include <math.h>
+
 
 
 
@@ -45,8 +47,240 @@ void affiche(rmat A){
         }
         printf("\n");
         
-    }
+    }}
     
 
+rmat add(rmat A, rmat B){
+    rmat C;
+    int i,j;
+    if (A.rown!=B.rown || A.coln!=B.coln)
+    {
+        printf("Erreur: les matrices ne sont pas de la même taille.\n");
+        exit(1);
+    }
+    C=init(A.rown, A.coln);
+    for ( i = 0; i < A.rown; i++)
+    {
+        for ( j = 0; j < A.coln; j++)
+        {
+            C.coeff[i][j]=A.coeff[i][j]+B.coeff[i][j];
+        }
+        
+    }
+    
+    return C;
+}
+rmat mult(rmat A, rmat B){
+    rmat C;
+    int i,j,k;
+    if (A.coln!=B.rown)
+    {
+        printf("Erreur: les matrices ne sont pas compatibles pour la multiplication.\n");
+        exit(1);
+    }
+    C=init(A.rown, B.coln);
+    for ( i = 0; i < A.rown; i++)
+    {
+        for ( j = 0; j < B.coln; j++)
+        {
+            C.coeff[i][j]=0;
+            for ( k = 0; k < A.coln; k++)
+            {
+                C.coeff[i][j]+=A.coeff[i][k]*B.coeff[k][j];
+            }
+            
+        }
+        
+    }
+    
+    return C;
+}
+rmat transposition(rmat A){
+    rmat T;
+    int i,j;
+    T=init(A.coln, A.rown);
+    for ( i = 0; i < A.rown; i++)
+    {
+        for ( j = 0; j < A.coln; j++)
+        {
+            T.coeff[j][i]=A.coeff[i][j];
+        }
+        
+    }
+    
+    return T;
 }
 
+
+int inverse(rmat A, rmat invA) {
+    if (A.rown != A.coln) {
+        printf("Erreur : La matrice n'est pas carrée, donc elle n'est pas inversible.\n");
+        return 0; // Retourne 0 pour indiquer une erreur
+    }
+
+    int n = A.rown;
+    rmat I = init_Id(n); // Matrice identité
+    rmat aug = init(n, 2 * n); // Matrice augmentée [A | I]
+    int i, j, k;
+
+    // Construire la matrice augmentée [A | I]
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            aug.coeff[i][j] = A.coeff[i][j]; // Partie A
+            aug.coeff[i][j + n] = I.coeff[i][j]; // Partie I
+        }
+    }
+
+    // Appliquer l'élimination de Gauss-Jordan
+    for (i = 0; i < n; i++) {
+        // Vérifier si le pivot est nul
+        if (fabs(aug.coeff[i][i]) < 1e-6) {
+            printf("Erreur : La matrice est singulière et ne peut pas être inversée.\n");
+            return 0; // Retourne 0 pour indiquer une erreur
+        }
+
+        // Normaliser la ligne i
+        float pivot = aug.coeff[i][i];
+        for (j = 0; j < 2 * n; j++) {
+            aug.coeff[i][j] /= pivot;
+        }
+
+        // Éliminer les autres lignes
+        for (k = 0; k < n; k++) {
+            if (k != i) {
+                float factor = aug.coeff[k][i];
+                for (j = 0; j < 2 * n; j++) {
+                    aug.coeff[k][j] -= factor * aug.coeff[i][j];
+                }
+            }
+        }
+    }
+
+    // Extraire la matrice inverse de la partie droite de la matrice augmentée
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            invA.coeff[i][j] = aug.coeff[i][j + n];
+        }
+    }
+
+    return 1; // Retourne 1 pour indiquer le succès
+}
+int rang(rmat A) {
+    int i, j, k;
+    int rank = 0;
+    rmat temp = init(A.rown, A.coln); // Copie de la matrice A
+
+    // Copier les éléments de A dans temp
+    for (i = 0; i < A.rown; i++) {
+        for (j = 0; j < A.coln; j++) {
+            temp.coeff[i][j] = A.coeff[i][j];
+        }
+    }
+
+    // Appliquer l'élimination de Gauss
+    for (i = 0; i < A.rown; i++) {
+        if (fabs(temp.coeff[i][i]) < 1e-6) {
+            continue; // Passer à la ligne suivante si le pivot est nul
+        }
+        rank++;
+        for (j = i + 1; j < A.rown; j++) {
+            float ratio = temp.coeff[j][i] / temp.coeff[i][i];
+            for (k = i; k < A.coln; k++) {
+                temp.coeff[j][k] -= ratio * temp.coeff[i][k];
+            }
+        }
+    }
+
+    return rank;
+}
+float det(rmat A) {
+    if (A.rown != A.coln) {
+        printf("Erreur : La matrice n'est pas carrée, donc le déterminant ne peut pas être calculé.\n");
+        return 0.0; // Retourne 0 pour indiquer une erreur
+    }
+
+    int n = A.rown;
+    rmat temp = init(n, n); // Copie de la matrice A
+    int i, j, k;
+    float determinant = 1.0;
+    float ratio;
+
+    // Copier les éléments de A dans temp
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            temp.coeff[i][j] = A.coeff[i][j];
+        }
+    }
+
+    // Appliquer l'élimination de Gauss
+    for (i = 0; i < n; i++) {
+        if (fabs(temp.coeff[i][i]) < 1e-6) {
+            printf("Erreur : La matrice est singulière, donc le déterminant est 0.\n");
+            return 0.0; // Retourne 0 si la matrice est singulière
+        }
+
+        for (j = i + 1; j < n; j++) {
+            ratio = temp.coeff[j][i] / temp.coeff[i][i];
+            for (k = i; k < n; k++) {
+                temp.coeff[j][k] -= ratio * temp.coeff[i][k];
+            }
+        }
+    }
+
+    // Calculer le déterminant comme le produit des pivots
+    for (i = 0; i < n; i++) {
+        determinant *= temp.coeff[i][i];
+    }
+
+    return determinant;
+}
+float trace(rmat A) {
+    if (A.rown != A.coln) {
+        printf("Erreur : La matrice n'est pas carrée, donc la trace ne peut pas être calculée.\n");
+        return 0.0; // Retourne 0 pour indiquer une erreur
+    }
+
+    float sum = 0.0;
+    for (int i = 0; i < A.rown; i++) {
+        sum += A.coeff[i][i]; // Ajouter les éléments de la diagonale principale
+    }
+
+    return sum;
+}
+rmat pivot(rmat A) {
+    rmat P = init(A.rown, A.coln); // Matrice pour stocker le résultat
+    int i, j, k;
+    float factor;
+
+    // Copier les éléments de A dans P
+    for (i = 0; i < A.rown; i++) {
+        for (j = 0; j < A.coln; j++) {
+            P.coeff[i][j] = A.coeff[i][j];
+        }
+    }
+
+    // Appliquer l'élimination de Gauss
+    for (i = 0; i < P.rown; i++) {
+        // Vérifier si le pivot est nul
+        if (P.coeff[i][i] == 0) {
+            printf("Erreur : Pivot nul détecté à la ligne %d.\n", i);
+            continue; // Passer à la ligne suivante
+        }
+
+        // Normaliser la ligne i
+        for (j = i + 1; j < P.coln; j++) {
+            P.coeff[i][j] /= P.coeff[i][i];
+        }
+        P.coeff[i][i] = 1;
+
+        // Éliminer les éléments en dessous du pivot
+        for (k = i + 1; k < P.rown; k++) {
+            factor = P.coeff[k][i];
+            for (j = i; j < P.coln; j++) {
+                P.coeff[k][j] -= factor * P.coeff[i][j];
+            }
+        }
+    }
+
+    return P;
+}
