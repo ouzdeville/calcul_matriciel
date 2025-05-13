754dd1a60aa33843d7fa1f02803955de063de63b
Ibrahima-Khalilou-llah-Sylla <khaliloullahsylla@gmail.com>
2025-05-13 23:14:24 +0000

    modification sur les fichier realmatrix.c et main.c

--- main.c
+++ main.c
@@ -3,33 +3,47 @@
 #include <time.h>
 #include "realmatrix.h"
 
-
 int main()
 {
-    rmat A,B,C;
-    A=init(2,2);
-    B=init(2,2);
-    int i,j;
-    srand( time( NULL ) );
-    for ( i = 0; i < A.rown; i++)
-    {
-        for ( j = 0; j < A.coln; j++)
-        {
-            A.coeff[i][j]=((int)rand())%2;
-            B.coeff[i][j]=((int)rand())%2;
+    rmat A, B, C, invA;
+    A = init(2, 2);
+    B = init(2, 2);
+    int i, j;
+
+    srand(time(NULL));
+    for (i = 0; i < A.rown; i++) {
+        for (j = 0; j < A.coln; j++) {
+            A.coeff[i][j] = ((int)rand()) % 2;
+            B.coeff[i][j] = ((int)rand()) % 2;
         }
-        
     }
-   
-    printf("A=\n");
+
+    printf("A =\n");
     affiche(A);
-    printf("B=\n");
+
+    printf("B =\n");
     affiche(B);
 
-    C=mult(A,B);
-    printf("C=\n");
+    printf("A + B =\n");
+    C = add(A, B);
     affiche(C);
-    
-    
-    return 1;
+
+    printf("A * B =\n");
+    C = mult(A, B);
+    affiche(C);
+
+    printf("Transposée de A =\n");
+    C = transposition(A);
+    affiche(C);
+
+    // Allocation vide, mais ce n’est plus indispensable
+    // car inverse() écrase le contenu avec init_Id()
+    if (inverse(A, &invA)) {
+        printf("Inverse de A :\n");
+        affiche(invA);
+    } else {
+        printf("La matrice A n'est pas inversible.\n");
+    }
+
+    return 0;
 }
